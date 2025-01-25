3374ddb49acdfc15ba6d8ae6410ada49baa42f01
anton <yushkevich.a.a@yandex.ru>
2025-01-25 11:10:56 +0300

    комментирование класса Config

--- Game/Config.h
+++ Game/Config.h
@@ -8,17 +8,19 @@ using json = nlohmann::json;
 class Config
 {
   public:
-    Config()
+    // круглые скобки для вызова функции конструктора и инициализации начальной конфигурации игры
+    Config() 
     {
         reload();
     }
 
-    void reload()
-    {
-        std::ifstream fin(project_path + "settings.json");
-        fin >> config;
-        fin.close();
-    }
+    // функция чтения конфигурации игры из файла settings.json и 
+    // сохранение в переменной config
+    void reload()     {
+        std::ifstream fin(project_path + "settings.json"); // открытие файла settings.json 
+        fin >> config; // чтение файла setting.json и сохранение конфига в переменную config
+        fin.close(); // закрытие файла settings.json
+    } 
 
     auto operator()(const string &setting_dir, const string &setting_name) const
     {
